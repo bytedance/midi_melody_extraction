106f1d9a6e96f9e9d0ad9cbacb47f9ae17c60c72
Katerina Kosta <119883301+katkost2@users.noreply.github.com>
2023-09-27 15:50:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 pip3 install -r requirements.txt
 
-python3 LStoM/process_data.py -i /Users/Katerina/Desktop/projects/MIDI_to_leadsheet_parser/POP909-Dataset/POP909 -o ./processed_data -q $@
+python3 LStoM/process_data.py -i __path_to__POP909-Dataset/POP909 -o ./processed_data -q $@
 python3 LStoM/create_stats_dict.py -i ./processed_data -o ./yaml_files $@
 python3 LStoM/train.py -sd ./yaml_files/stats_config_train_valid.yaml -ds ./yaml_files/data_split.yaml -o ./model_results -of ./model_results/test_model.pt -v $@
